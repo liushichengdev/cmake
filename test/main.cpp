f4e9af7e7b08bc98c9db46bf4722130d7f752cfa
liushicheng <liushichengdev@163.com>
2023-12-08 16:46:08 +0800

    Try add_definitions()

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "Adder.h"
+#include <fmt/core.h>
 
 int main() 
 {
     std::cout << adder(12, 12);
     std::cout<<"Hello, world!\n";
+    fmt::print("Hello, fmt\n");
+#ifdef DEBUG
+    fmt::print("Hello, Debug\n");
+#endif// DEBUG
+#ifdef RELEASE
+    fmt::print("Hello, Release\n");
+#endif // RELEASE
+
     return 0;
 }
