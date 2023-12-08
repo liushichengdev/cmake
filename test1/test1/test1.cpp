86ace52825b33627ee41dcbcf9124a6a01c8eb15
liushicheng <liushichengdev@163.com>
2023-12-08 16:47:41 +0800

    Add test1

---
+++ test1/test1/test1.cpp
@@ -0,0 +1,13 @@
+﻿// test1.cpp : Defines the entry point for the application.
+//
+
+#include "test1.h"
+#include <fmt/core.h>
+using namespace std;
+
+int main()
+{
+	fmt::print("Don't {}\n", "panic");
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ test1/test1/test1.h
@@ -0,0 +1,9 @@
+﻿// test1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+//#include <fmt/format.h>
