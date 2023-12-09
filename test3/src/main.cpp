94dcb5c45d92668167d1a061ee6da7a4a0998adb
liushicheng <liushichengdev@163.com>
2023-12-09 17:43:08 +0800

    Add test3

---
+++ test3/src/main.cpp
@@ -0,0 +1,12 @@
+﻿// test3.cpp : Defines the entry point for the application.
+//
+
+#include "static.h"
+
+using namespace std;
+
+int main()
+{
+	static_print();
+	return 0;
+}
---
+++ test3/src/static.cpp
@@ -0,0 +1,6 @@
+#include "static.h"
+
+void static_print(void)
+{
+	printf("static hello: cmaker\r\n");
+}
